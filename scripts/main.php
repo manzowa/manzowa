47b16ebfcef807fea3105fc87aa1ef1d2b56ce59
Christian Shungu <christianshungu@gmail.com>
2025-09-05 00:53:56 +0200

    Catch Error in main

--- scripts/main.php
+++ scripts/main.php
@@ -3,5 +3,12 @@ define('DS', DIRECTORY_SEPARATOR);
 define('__ROOT__', dirname(__DIR__));
 require_once join(DS, [__ROOT__, 'vendor', 'autoload.php']);
 require_once join(DS, [__ROOT__, 'scripts', 'AppRun.php']);
-\Dotenv\Dotenv::createUnsafeImmutable(__ROOT__)->load();
+try {
+    \Dotenv\Dotenv::createUnsafeImmutable(__ROOT__)->load();
+} catch (\Dotenv\Exception\InvalidPathException $e) {
+    $msg = "❌ Warning: .env file not found at " . __ROOT__ . '/.env';
+    echo $msg . PHP_EOL;
+    error_log($msg);
+    die();
+}
 AppRun::setUp();
